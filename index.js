c5f6dcc8c2b4b03efa4c19259acaa3596a09420c
akku-18 <akshanshgarg18@gmail.com>
2023-01-11 22:20:18 +0530

    Dice game

---
+++ index.js
@@ -0,0 +1,22 @@
+var random1 = Math.floor(Math.random(5)*6)+1;
+
+var source1 = "images/dice" + random1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", source1 );
+
+
+var random2 = Math.floor(Math.random(5)*6) + 1;
+
+var source2 = "images/dice" + random2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", source2);   
+
+if (random1 > random2){
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+else if(random2 > random1){
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
